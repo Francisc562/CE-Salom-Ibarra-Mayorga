20cd3cc843c1a3193714646ba32ed27d7fa15d79
kener <poncekenerkjp@gmail.com>
2019-12-13 00:00:12 -0600

    Diseño de Login

---
+++ CE-Salom-Ibarra-Mayorga/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CESIM.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CESIM.Areas.Identity.IdentityHostingStartup))]
+namespace CESIM.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
